b918701ca20014918dc09a30e25579ed90167d72
joonion <joonion@gmail.com>
2021-08-01 10:05:28 +0900

    Flame Coding

--- FlameCoding/Mission.1.Triangle.py
+++ FlameCoding/Mission.1.Triangle.py
@@ -1,9 +1,19 @@
-import turtle
+import turtle as t
+import time
 
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+t.title("Rectangle")
+t.setup(540, 960)
+t.shape("turtle")
+t.bgcolor("white")
+t.pencolor("red")
+t.fillcolor("red")
+t.pensize(2)
+t.speed(1)
+t.showturtle()
+input("Press any key to continue...")
+
+for i in range(4):
+    t.forward(200)
+    t.right(90)
+
+input("Press any key to exit...")
