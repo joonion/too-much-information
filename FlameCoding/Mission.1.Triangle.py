2213025b5ab7f214714ba5eb74a2f0fa327b267c
Joonion Bae <joonion@gmail.com>
2021-08-03 11:25:14 +0900

    Sierpinski Triangle

--- FlameCoding/Mission.1.Triangle.py
+++ FlameCoding/Mission.1.Triangle.py
@@ -1,9 +1,15 @@
-import turtle
+import turtle as t
+import time 
 
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+t.forward(100)
+t.right(90)
+time.sleep(0.5)
+t.forward(100)
+t.right(90)
+time.sleep(0.5)
+t.forward(100)
+t.right(90)
+time.sleep(0.5)
+t.forward(100)
+
+input("Press any key to exit ...")
---
+++ FlameCoding/SierpinskiTriangle.py
@@ -0,0 +1,11 @@
+import turtle as t
+
+def triangle(size):
+    t.forward(size)
+    t.right(120)
+
+if __name__=='__main__':
+    t.right(30)
+    triangle(200)
+    input("Press any key...")
+
